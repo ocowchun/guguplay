5409f08aeed9467835635fdec26a26a6ccfbab7d
ocowchun <ocowchun@gmail.com>
2014-01-13 23:23:06 +0800

    New:import bootsrap to main.css

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Guguplay::Application.routes.draw do
   devise_for :users
+  resources :trips
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -40,7 +42,7 @@ Guguplay::Application.routes.draw do
   #       get 'recent', on: :collection
   #     end
   #   end
-  
+
   # Example resource route with concerns:
   #   concern :toggleable do
   #     post 'toggle'
