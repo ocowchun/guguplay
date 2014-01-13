65afa0610f4d7820f798100cc627e963f50643a0
ocowchun <ocowchun@gmail.com>
2014-01-13 23:08:51 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Guguplay::Application.config.secret_key_base = '15c44bf1097cb244d71f71ff59d4221f659d1d424f617f46cf938d0fed9dadd43a1a848d48f61357ad914d253ac72dc6b0555c0a5268c4f1b4462bc60ee7888c'
