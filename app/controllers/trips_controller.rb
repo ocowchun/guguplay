84c31553123f9bbbec3323da6261f336cbbf114c
ocowchun <ocowchun@gmail.com>
2014-01-13 23:37:27 +0800

    New:trip controller new,create,show

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,2 +1,26 @@
 class TripsController < ApplicationController
+  def new
+    @trip=Trip.new
+  end
+  def create
+    @trip=Trip.new(trip_params)
+    if @trip.save
+      redirect_to trip_path(@trip)
+    else
+      render :new
+    end
+  end
+
+  def show
+find_trip
+  end
+
+  private
+  def find_trip
+    @trip=Trip.find(params[:id])
+  end
+
+  def trip_params
+    params.require(:trip).permit(:name)
+  end
 end
